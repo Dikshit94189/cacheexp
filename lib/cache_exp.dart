33a5a5e3a83095380d35b1e255f4d85a15b94244
Dikshit Sharma <noreply@feedback.github.com>
2026-03-07 17:32:17 +0530

    add code inside class

--- lib/cache_exp.dart
+++ lib/cache_exp.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CacheExampel extends StatefulWidget {
@@ -8,10 +9,29 @@ class CacheExampel extends StatefulWidget {
 }
 
 class _CacheExampelState extends State<CacheExampel> {
+   final String imageUrl =
+      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Offline Image Cache"),
+      ),
+      body: Center(
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          width: 300,
+          height: 300,
+          fit: BoxFit.cover,
 
+          placeholder: (context, url) =>
+              CircularProgressIndicator(),
+
+          errorWidget: (context, url, error) =>
+              Icon(Icons.error, size: 40),
+        ),
+      ),
     );
   }
 }
